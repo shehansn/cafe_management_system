6b94c7a97bef68192c862e51fd074defa22c0279
shehan_n <shehan_n@epiclanka.net>
2023-10-24 11:48:09 +0530

    Initial Project Setup Git repo

---
+++ src/main/java/com/sn/shehan_n/cafe_management_system/CafeManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sn.shehan_n.cafe_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeManagementSystemApplication.class, args);
+	}
+
+}
